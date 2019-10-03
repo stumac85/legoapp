b85aa152963da04a308e380f961608ec84c2037e
Stuart Mackie <stumac85@hotmail.com>
2019-10-03 16:45:16 +0100

    Updated the layout and navbar so that only the completed Lego App section is visible

--- resources/views/layout/partials/nav.blade.php
+++ resources/views/layout/partials/nav.blade.php
@@ -3,31 +3,7 @@
 	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#mainNavbar" aria-controls="mainNavbar" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-	<a class="navbar-brand" href="/">Stuart Mackie</a>
-	
-	<div class="collapse navbar-collapse" id="mainNavbar">
-    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-			<li class="nav-item">
-				<a class="nav-link" href="#">About</a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link" href="#">Portfolio</a>
-			</li>
-			<li class="nav-item dropdown">
-				<a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Samples</a>
-				<div class="dropdown-menu">
-					<a class="dropdown-item" href="/samples/legoapp">Lego Search App</a>
-					<a class="dropdown-item" href="#">Another action</a>
-					<a class="dropdown-item" href="#">Something else here</a>
-					<div class="dropdown-divider"></div>
-					<a class="dropdown-item" href="#">Separated link</a>
-				</div>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link" href="#">Contact</a>
-			</li>
-    </ul>
-  </div>
+	<a class="navbar-brand" href="/">Lego App</a>
   
 </ul>
     </header>
--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@
 
 
 
-Route::get('/', 'test');
+Route::get('/', 'legoapp@index');
 
 Route::get('/about', function () {
     return view('about');
@@ -21,7 +21,6 @@ Route::get('/about', function () {
 
 Route::get('/samples/legoapp', 'legoapp@index');
 Route::post('/samples/legoapp/addItem', 'legoapp@addItem');
-Route::get('/samples/legoapp/sessionhell', 'legoapp@sessionHell');
 Route::post('/samples/legoapp/removeItem', 'legoapp@removeItem');
 Route::post('/samples/legoapp/getSets', 'legoapp@getSets');
 Route::get('/samples/legoapp/print/{id}', 'legoapp@pdf');
